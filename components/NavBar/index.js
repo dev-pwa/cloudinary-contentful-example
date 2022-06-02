e1e7f2557a3aab5278f1a6071ab65b6efd9be06c
Elad Rosenheim <elad@stackbit.com>
2022-06-02 11:02:57 +0300

    Ensure home navbar item is first

--- components/NavBar/index.js
+++ components/NavBar/index.js
@@ -7,6 +7,11 @@ import styles from "./index.module.css";
   This component is not driven by a content model (yet! leaving this as an exercise to the reader...)
 */
 export function NavBar({ label, navbarItems }) {
+  const homeItem = navbarItems.find(item => item.path === "/");
+  if (homeItem) {
+    const restOfItems = navbarItems.filter(item => item.path !== "/");
+    navbarItems = [homeItem, ...restOfItems];
+  }
   return (
     <div className={styles.wrapper}>
       <img className={styles.logoImage} src="/sb-cld-logo.svg" alt="Logo" />
